48d80569467cdc6ba4ee34aec71e68311f6dba8b
Munezero26200 <alinemunezero920@gmail.com>
2025-09-02 14:20:33 +0200

    here a navbar so far

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,15 @@
 export default function Navbar() {
   return (
     <div className="h-16 flex justify-between items-center px-20">
-      <div className="flex items-center space-x-4">
-        <img src="/src/assets/image/logo.png" alt="logo" class="h-16 rounded-2xl" />
-      <h1 className="text-2xl font-bold font-serif text-yellow-500">BRIDGE2RISE</h1>
+      <div className="flex items-center space-x-2">
+        <img src="/src/assets/image/logo.png" alt="logo" class="h-16 rounded-2xl mt-3" />
+      <h1 className="text-2xl font-bold font-serif">BRIDGE2RISE</h1>
       </div>
       <div className="flex space-x-16 font-semibold font-serif">
         <p>Home</p>
-        <p>Profile</p>
-        <p>Message</p>
-        <p>Calendar</p>
-        <p>Resources</p>
+        <p>About</p>
+        <p>Mentor</p>
+        <p>Mentee</p>
       </div>
     </div>
   );
