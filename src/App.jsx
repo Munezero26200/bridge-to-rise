a92a640f57089f3cf847b8dad9ea767c14269850
Reponse <reponse.iduha2023@kepler.org>
2025-09-03 17:35:53 +0200

    Created a dashboard for a mentor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar"
 import Hero from './components/hero'
+import MentorDashboard from './components/mentorDashboard'
 
 export default function App() {
   return (
    <>
     <Navbar />
     < Hero/>
+    <MentorDashboard />
    </>
   )
 }
