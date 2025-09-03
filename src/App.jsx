7c0b50b359a438e36f2e50fd1366fe621b3d6f00
Reponse <reponse.iduha2023@kepler.org>
2025-09-03 17:41:40 +0200

    Created a mentee dashboard onits own branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar"
 import Hero from './components/hero'
+import MenteeDashboard from "./menteeDashboard"
 
 export default function App() {
   return (
    <>
     <Navbar />
     < Hero/>
+    <MenteeDashboard />
    </>
   )
 }
